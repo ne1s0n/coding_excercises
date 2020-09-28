e5624ac1263b608645df24490d91084dcd5dfd86
filippob <filippo.biscarini@gmail.com>
2020-09-28 09:15:03 +0200

    stand-alone scripts to train adn test DL on MNIST data

---
+++ lab_day1/keras.mnist_test.py
@@ -0,0 +1,68 @@
+"""
+Script to evaluate the performnce
+of a trained deep learning model for image recognition
+on independent test data
+"""
+
+
+"""
+Import libraries
+"""
+import keras
+import keras.utils
+from keras import backend as K ## needed? What for?
+from keras.datasets import mnist
+from keras.models import model_from_json
+import numpy
+
+"""
+Configuration parameters
+"""
+num_classes = 10
+img_rows, img_cols = 28, 28
+
+"""
+Test data
+"""
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print("Size of the test set")
+print(X_test.shape)
+
+"""
+Data preprocessing
+"""
+K.image_data_format()
+
+if K.image_data_format() == 'channels_first':
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+X_test = X_test.astype('float32')
+X_test /= 255
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+"""
+Test model trained for image recognition
+"""
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+ 
+# evaluate loaded model on test data
+loaded_model.compile(
+	loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adadelta(),
+	metrics=['accuracy'])
+
+score = loaded_model.evaluate(X_test, y_test, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
---
+++ lab_day1/keras.mnist_train.py
@@ -0,0 +1,119 @@
+###################################################
+## script to train a DL model for image recognition
+###################################################
+
+"""
+import libraries
+"""
+import keras.utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K ##
+from keras.datasets import mnist
+from keras.models import model_from_json
+import numpy
+
+"""
+Configuration parameters
+"""
+batch_size = 128 ## n. of samples/records in each batch
+num_classes = 10
+epochs = 12
+
+# input image dimensions (pixels)
+img_rows, img_cols = 28, 28
+
+"""
+Input data
+"""
+# the data, split between train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train[0:3000,]
+y_train = y_train[0:3000]
+
+print("Size of the training set")
+print(X_train.shape)
+print("Size of the test set")
+print(X_test.shape)
+
+"""
+Data preprocessing
+"""
+K.image_data_format()
+
+if K.image_data_format() == 'channels_first':
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+print("Modified array dimensions:")
+print(X_train.shape)  
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print('x_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+print(y_train[0])
+
+"""
+Model building
+"""
+model = Sequential()
+model.add(
+          Conv2D(32, kernel_size=(3, 3),
+          activation='relu',
+          input_shape=input_shape))
+
+
+print(model.output_shape)
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+"""
+Compiling the model
+"""
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+"""
+Training the model
+"""
+model.fit(X_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1)
+
+"""
+Save out the trained model
+"""
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+
+# serialize weights to HDF5
+model.save_weights("model.h5")
+
+print("Saved model to disk")
+
